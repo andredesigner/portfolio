e97a9c53dad043279f2a16062b724f9d0c82ee0e
André Oliveira <andresoliveira@hotmail.com.br>
2024-06-24 21:04:39 -0300

    fix the contact me form

--- pages/fale-comigo.vue
+++ pages/fale-comigo.vue
@@ -12,18 +12,18 @@
         <p>Fique a vontade para me mandar mensagem e marcar uma conversa ou um café, será um prazer conhece-lo e me apresentar.</p>
         <form>
           <div class="ao-input radius-top-left-right-10">
+            <input type="text" class="ao-form-control form-control shadow-none" id="name" required>
             <label for="name" class="ao-form-label form-label">Como costumam te chamar</label>
-            <input @click="HandlerFunction" type="text" class="ao-form-control form-control shadow-none" id="name">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
           </div>
           <div class="ao-input">
+            <input type="email" class="ao-form-control form-control shadow-none" id="e-mail" required>
             <label for="e-mail" class="ao-form-label form-label">Um e-mail para eu retornar</label>
-            <input type="email" class="ao-form-control form-control shadow-none" id="e-mail" aria-describedby="emailHelp">
             <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
           </div>
           <div class="ao-input radius-bottom-left-right-10">
+            <textarea for="message" class="ao-form-control ao-textarea form-control shadow-none" id="message" contenteditable required></textarea>
             <label for="message" class="ao-form-label form-label">Você gosta de café?</label>
-            <textarea for="message" class="ao-form-control ao-textarea form-control shadow-none" id="message" contenteditable></textarea>
             <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
           </div>
           <button type="submit" class="btn ao-btn">Enviar</button>
@@ -34,7 +34,7 @@
 </template>
 
 <style scoped>
-.ao-btn{
-  margin-top: 40px;
-}
+  .ao-btn{
+    margin-top: 40px;
+  }
 </style>
