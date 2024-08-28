bbab2e31682be24136d84e7a2706219132f7f944
André Oliveira <andresoliveira@hotmail.com.br>
2024-08-28 10:43:49 -0300

    add view social media in mobile

--- components/AoHeader.vue
+++ components/AoHeader.vue
@@ -35,7 +35,7 @@
                                 <nuxt-link to="https://www.linkedin.com/in/andreoliveiradesigner/" target="_blank">Linkedin</nuxt-link>
                             </li>
                         </ul>
-                        <span v-on:click="toggleActive" @click="isActive=!isActive" class="view-social-media">+</span>
+                        <div v-on:click="toggleActive" @click="isActive=!isActive" class="view-social-media"></div>
                     </div>
                 </div>
             </div>
@@ -70,20 +70,16 @@
     }
 
     .view-social-media{
-        width: 44px;
-        height: 44px;
-        font-size: 36px;
+        width: 52px;
+        height: 52px;
+        background-image: url("data:image/svg+xml,%3Csvg%20width%3D%2252%22%20height%3D%2252%22%20viewBox%3D%220%200%2052%2052%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Crect%20width%3D%2252%22%20height%3D%2252%22%20rx%3D%2226%22%20fill%3D%22%23FFB607%22%2F%3E%3Cpath%20fill-rule%3D%22evenodd%22%20clip-rule%3D%22evenodd%22%20d%3D%22M28%2018C28%2016.8954%2027.1046%2016%2026%2016V16C24.8954%2016%2024%2016.8954%2024%2018V20C24%2022.2091%2022.2091%2024%2020%2024L18%2024C16.8954%2024%2016%2024.8954%2016%2026V26C16%2027.1046%2016.8954%2028%2018%2028H20C22.2091%2028%2024%2029.7909%2024%2032V34C24%2035.1046%2024.8954%2036%2026%2036V36C27.1046%2036%2028%2035.1046%2028%2034V32C28%2029.7909%2029.7909%2028%2032%2028H34C35.1046%2028%2036%2027.1046%2036%2026V26C36%2024.8954%2035.1046%2024%2034%2024L32%2024C29.7909%2024%2028%2022.2091%2028%2020V18Z%22%20fill%3D%22%235B4A20%22%2F%3E%3C%2Fsvg%3E");
         padding: 0;
         margin: 0;
-        line-height: 1;
-        text-align: center;
         transition: all 0.2s ease-in-out;
         position: fixed;
         bottom: 40px;
         right: 40px;
-        display: inline-block;
-        background-color: #FFB607;
-        border-radius: 44px;
+        display: block;
 
         @media (min-width: 993px) {
             display: none;
